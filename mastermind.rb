9d93a3599ba4dec621977ea90d31609eba0f3e2e
Marcin Galiński <marcin.galinski95@gmail.com>
2015-12-04 00:08:52 +0100

    implement new game initiation

--- mastermind.rb
+++ mastermind.rb
@@ -1,3 +1,36 @@
-colors = %w(blue green red yellow white black)
-chosen_colors = Array.new(4) { |i| colors[rand(0...6)] } 
-puts chosen_colors.join
+class Game
+	def play
+		greet
+		10.times do |i|
+			puts "#{10 - i} attempts remaining"
+			puts check_guess(take_guess)
+		end
+
+	end
+
+	private
+
+	def greet
+		puts "I've chosen four colors between #{colors.join(', ')}.\n" \
+				 "You have 10 attempts to guess them. In the right order!"
+	end
+
+	def colors
+		colors = %w(blue green red yellow white black)
+	end
+
+	def choose_colors
+		@chosen_colors = Array.new(4) { |i| colors[rand(0...6)] } 
+	end
+
+	def take_guess
+		puts 
+	end
+
+	def check_guess guess
+		
+	end
+end
+
+game = Game.new
+game.play
