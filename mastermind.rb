5104e690b32024c7036064b2343f158abf817e22
Marcin Galiński <marcin.galinski95@gmail.com>
2015-12-04 00:26:50 +0100

    game takes user input

--- mastermind.rb
+++ mastermind.rb
@@ -1,11 +1,19 @@
 class Game
+	def initialize
+		@win = false	
+	end
+
 	def play
 		greet
+
 		10.times do |i|
 			puts "#{10 - i} attempts remaining"
-			puts check_guess(take_guess)
+			check_guess take_guess
+			break if @win
+			give_hint
 		end
 
+		give_result
 	end
 
 	private
@@ -24,11 +32,20 @@ class Game
 	end
 
 	def take_guess
-		puts 
+		puts "Let's try!"
+		gets.chomp.split
 	end
 
 	def check_guess guess
 		
+	end
+
+	def give_hint
+		
+	end
+
+	def give_result
+		puts @win ? "You won!" : "You lost!"
 	end
 end
 
