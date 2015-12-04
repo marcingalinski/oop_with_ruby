7776b2a0a7765020c3423ce1bd664c4b2ab75335
Marcin Galiński <marcin.galinski95@gmail.com>
2015-12-04 12:26:51 +0100

    implement win and loose
    game is working now

--- mastermind.rb
+++ mastermind.rb
@@ -1,6 +1,8 @@
 class Game
 	def initialize
-		@win = false	
+		@win = false
+		@chosen_colors = choose_colors
+		@hint = "Let's try!"
 	end
 
 	def play
@@ -8,9 +10,9 @@ class Game
 
 		10.times do |i|
 			puts "#{10 - i} attempts remaining"
-			check_guess take_guess
-			break if @win
+			check_guess
 			give_hint
+			break if @win
 		end
 
 		give_result
@@ -28,20 +30,58 @@ class Game
 	end
 
 	def choose_colors
-		@chosen_colors = Array.new(4) { |i| colors[rand(0...6)] } 
+		Array.new(4) { |i| colors[rand(0...6)] } 
 	end
 
 	def take_guess
-		puts "Let's try!"
-		gets.chomp.split
+		guessed_colors = nil
+
+		until @win
+			guessed_colors = gets.chomp.split
+
+			if guessed_colors.each { |color| colors.include? color } && guessed_colors.size == 4
+				break			
+			else
+				puts "Please input 4 colors"
+				redo
+			end
+		end
+
+		guessed_colors
 	end
 
-	def check_guess guess
+	def check_guess
+		guessed = 0
+		misplaced = 0
+		chosen = [] + @chosen_colors
+
+		take_guess.each_with_index do |color, index|
+			if chosen[index] == color
+				guessed += 1
+				chosen[index] = "match"
+			elsif chosen.include? color
+				misplaced += 1 
+				chosen[chosen.index(color)] = "match"
+			end
+		end
 		
+		update_status(guessed, misplaced)
+	end
+
+	def update_status(guessed, misplaced)
+		if guessed == 4
+			@win = true
+			@hint = "You guessed all of the colors!"
+		elsif misplaced == 0
+			@hint = "You guessed #{guessed} colors exactly."
+		else		
+			@hint = "You guessed #{guessed} colors exactly,\n" \
+							"#{misplaced} colors was correct, but misplaced."
+		end
 	end
 
 	def give_hint
-		
+		puts @hint
 	end
 
 	def give_result
